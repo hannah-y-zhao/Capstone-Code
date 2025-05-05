934ed50b083bb5e752d2df6d1f8caa05f5a26eac
hannahyuwen <hz2788@nyu.edu>
2025-05-05 05:31:00 -0400

    added other display page

---
+++ displayAll.js
@@ -0,0 +1,99 @@
+let panel=document.getElementById("panel")
+let container=document.getElementById("container")
+let heading=document.getElementById("header-text")
+let backButton=document.getElementById("back")
+
+let peopleCategory="#93c5fd"
+let thingsCategory="#fde047"
+let fireCategory="#fdba74"
+let craftCategory="#a3e635"
+let wormCategory="#f472b6"
+let verbsCategory="#d8b4fe"
+
+function getColor(category){
+    if(category=="people"){
+        return peopleCategory;
+      }else if(category=="things"){
+        return thingsCategory;
+      }else if(category=="verbs"){
+        return verbsCategory;
+      }else if(category=="craft"){
+        return craftCategory;
+      }else if(category=="fire"){
+        return fireCategory;
+      }else if(category=="worm"){
+        return wormCategory;
+      }
+}
+
+function display() {
+    getFirebase((data) => {
+      currentData = data.database;
+      for (let i = 0; i < currentData.length; i++) {
+        const miniCanvas = document.createElement("canvas");
+        miniCanvas.classList.add("averageImagesCanvas")
+        miniCanvas.dataset.word=currentData[i].word
+        miniCanvas.dataset.category=currentData[i].category
+        miniCanvas.classList.add("box-border")
+  
+        miniCanvas.width = 300; 
+        miniCanvas.height = 300;
+        const ctx = miniCanvas.getContext("2d");
+        ctx.globalCompositeOperation ="screen"
+  
+        const wordName=document.createElement("div")
+        wordName.innerHTML=`[${currentData[i].word}]`
+        wordName.classList.add("text-center")
+  
+        const canvasWrapper = document.createElement("div");
+        canvasWrapper.classList.add("w-fit")
+        canvasWrapper.classList.add("h-fit")
+        canvasWrapper.classList.add("gap-2")
+        canvasWrapper.style.backgroundColor=getColor(currentData[i].category)
+        canvasWrapper.appendChild(miniCanvas);
+        canvasWrapper.appendChild(wordName);
+        container.appendChild(canvasWrapper);
+        if (currentData[i].urls) {
+          let keys = Object.keys(currentData[i].urls);
+          keys.forEach((key) => {
+            const value = currentData[i].urls[key];
+            const img = new Image();
+            img.style.opacity=1/(keys.length)
+            img.src = value;
+            img.onload = () => {
+              ctx.drawImage(img, 0, 0, 300, 300);
+            };
+          });
+          ctx.rect(0,0,300,300);
+          ctx.fillStyle = getColor(currentData[i].category);
+          ctx.fill();
+        }
+        canvasWrapper.addEventListener("click",(e)=>{
+                let keys = Object.keys(currentData[i].urls);
+                keys.forEach((key) => {
+                    const value = currentData[i].urls[key];
+                    const img = document.createElement("img");
+                    img.src = value;
+                    img.style.width="30%"
+                    img.style.height="auto"
+                    // img.style.height="100px"
+                    panel.appendChild(img);
+
+                });
+            heading.innerHTML=`These are all previous creations of <div class="inline" style="background-color:${getColor(currentData[i].category)}"> [${currentData[i].word}]</div> `
+            console.log(currentData[i].urls)
+            panel.classList.remove("hidden")
+            panel.classList.add("flex")
+            container.classList.remove("flex")
+            container.classList.add("hidden")
+            backButton.classList.remove("hidden")
+        })
+      }
+    });
+  }
+  backButton.addEventListener("click",()=>{
+    location.reload()
+  })
+  window.addEventListener("load",()=>{
+    display()
+  })
